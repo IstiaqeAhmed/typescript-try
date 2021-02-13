bb427aa45f29475b4224ccfc86991802bcd12508
Istiaqe Ahmed Khan <istiaqeahmed.cse@gmail.com>
2021-02-13 19:32:35 +0600

    arrow function typescript

--- dist/first.js
+++ dist/first.js
@@ -1,4 +1,23 @@
 "use strict";
-let myMoney = 50;
-myMoney = 10;
-myMoney = 0;
+let age = 89;
+age = 59;
+let club = 'RM';
+const isFamous = false;
+let famous;
+// function add(num1: number | string, num2: number | string) {
+//     return num1 + num2;
+// }
+// add(3, 76);
+// add(52, 'sand');
+function doubleItAndConsole(num) {
+    const result = num * 2;
+    console.log(result);
+}
+const output = doubleItAndConsole(10);
+console.log('output', output);
+function fullName(firstName, lastName) {
+    return firstName + ' ' + lastName;
+}
+const user = fullName('Ben', 'Ahmed');
+const multiply = (x, y) => x * y;
+console.log(multiply(235, 6));
--- first.ts
+++ first.ts
@@ -5,12 +5,20 @@ const isFamous: boolean = false;
 let famous: boolean;
 
 
-function add(num1: number | string, num2: number | string) {
-    return num1 + num2;
+// function add(num1: number | string, num2: number | string) {
+//     return num1 + num2;
 
+// }
+// add(3, 76);
+// add(52, 'sand');
+
+function doubleItAndConsole(num: number): void {
+    const result = num * 2;
+    console.log(result);
 }
-add(3, 76);
-add(52, 'sand');
+
+const output = doubleItAndConsole(10);
+console.log('output', output);
 
 
 function fullName(firstName: string, lastName: string): string {
@@ -18,3 +26,14 @@ function fullName(firstName: string, lastName: string): string {
 }
 
 const user: string = fullName('Ben', 'Ahmed');
+
+
+
+
+
+let multiply2: (x: number, y: number) => number;
+
+multiply2 = (x, y) => x * y;
+
+const multiply = (x: number, y: number): number => x * y;
+console.log(multiply(235, 6)
